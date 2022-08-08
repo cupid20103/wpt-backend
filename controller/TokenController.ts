a1e9daa62bc0dde8489a95ceab0e4d7080957040
Piranha20103 <webphantom20103@gmail.com>
2022-08-08 09:10:11 +0900

    fix: update coinmarketcap api option

--- controller/TokenController.ts
+++ controller/TokenController.ts
@@ -4,7 +4,7 @@ import { COIN_MARKET_CAP_APIKEY } from "../config/env";
 const getInfo = (req: any, res: any) => {
   axios
     .get(
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=6000&&limit=5000&&sort=name",
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=8000&&limit=5000&&sort=name",
       {
         headers: {
           "X-CMC_PRO_API_KEY": COIN_MARKET_CAP_APIKEY,
